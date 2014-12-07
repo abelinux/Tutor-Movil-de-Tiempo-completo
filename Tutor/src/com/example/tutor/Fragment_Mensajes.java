3df8026b4d3f5b51c17ef243a2b94bd03351d41e
u19022013 <u19022013@LENOVO-PC>
2014-12-07 13:38:17 -0600

    version ya operable

--- Tutor/src/com/example/tutor/Fragment_Mensajes.java
+++ Tutor/src/com/example/tutor/Fragment_Mensajes.java
@@ -41,24 +41,35 @@ public class Fragment_Mensajes extends Fragment {
 	Button Btngetdata;
 	ArrayList<HashMap<String, String>> oslist = new ArrayList<HashMap<String, String>>();
 	//URL to get JSON Array
-	private static String url;
+	private static String url, url_mensaje;
 	//JSON Node Names
 	private static final String TAG_OS = "mensajes";
 	private static final String TAG_VER = "a";
 	private static final String TAG_NAME = "b";
 	private static final String TAG_API = "c";
 	JSONArray lista_tutorados = null;
-	String id_tutor, nombre, cct, id_alumno; 
+	String id_tutor, nombre, cct, id_alumno;
+	String respuesta;
+	ListAdapter adapter;
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		rootView = inflater.inflate(R.layout.fm_mensajes, container, false);
 		list=(ListView) rootView.findViewById(R.id.list);
 		mensaje = (EditText) rootView.findViewById(R.id.mensaje);
 		enviar = (Button) rootView.findViewById(R.id.enviar);
 		id_alumno = getActivity().getIntent().getStringExtra("id_alumno");
-		
+		id_tutor = getActivity().getIntent().getStringExtra("id_tutor");
 		url =  getString(R.string.url)+"mensajes.php";
+		url_mensaje=  getString(R.string.url)+"newMensaje.php";
 		oslist = new ArrayList<HashMap<String, String>>();
 		new JSONParse().execute();
+		
+		enviar.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				new JSONEnviarMensaje().execute();
+			}
+		});
+		
 		return rootView;
 	}
 	
@@ -70,6 +81,7 @@ public class Fragment_Mensajes extends Fragment {
              ver = (TextView) rootView.findViewById(R.id.nombre);
              name = (TextView) rootView.findViewById(R.id.plantel);
              api = (TextView) rootView.findViewById(R.id.nivel);
+             list.invalidateViews();
              pDialog = new ProgressDialog(rootView.getContext());
              pDialog.setMessage("Consultando los mensajes ...");
              pDialog.setIndeterminate(false);
@@ -89,22 +101,25 @@ public class Fragment_Mensajes extends Fragment {
         pDialog.dismiss();
         try {
             lista_tutorados = json.getJSONArray(TAG_OS);
-            for(int i = 0; i < lista_tutorados.length(); i++){
-            	JSONObject c = lista_tutorados.getJSONObject(i);
-	            String ver = c.getString(TAG_NAME);
-	            String name = c.getString(TAG_VER);
-	            String api = c.getString(TAG_API);
-	            HashMap<String, String> map = new HashMap<String, String>();
-	            map.put(TAG_VER, ver);
-	            map.put(TAG_NAME, name);
-	            // map.put(TAG_API, api);
-	            oslist.add(map);
-	            list=(ListView) rootView.findViewById(R.id.list);
-	            ListAdapter adapter = new SimpleAdapter(rootView.getContext(), oslist,
-                R.layout.list_v,
-                	new String[] { TAG_VER,TAG_NAME, TAG_API }, new int[] {
-	                    R.id.nombre,R.id.plantel, R.id.nivel});
-	            		list.setAdapter(adapter);
+            	for(int i = 0; i < lista_tutorados.length(); i++){
+	            	JSONObject c = lista_tutorados.getJSONObject(i);
+		            String ver = c.getString(TAG_NAME);
+		            String name = c.getString(TAG_VER);
+		            String api = c.getString(TAG_API);
+		            HashMap<String, String> map = new HashMap<String, String>();
+		            map.put(TAG_VER, ver);
+		            map.put(TAG_NAME, name);
+		            // map.put(TAG_API, api);
+		            oslist.add(map);
+		            
+		            list=(ListView) rootView.findViewById(R.id.list);
+		            
+		            adapter = new SimpleAdapter(rootView.getContext(), oslist,
+	                R.layout.list_v,
+                		new String[] { TAG_VER,TAG_NAME, TAG_API }, new int[] {R.id.nombre,R.id.plantel, R.id.nivel});
+			            // adapter.invalidateViews();
+			            
+			            list.setAdapter(adapter);
 	            		list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             			@Override
 	                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -112,9 +127,54 @@ public class Fragment_Mensajes extends Fragment {
 	                    }
 	                });
             	}
+            	
         	} catch (JSONException e) {
         		e.printStackTrace();
         	}
        }
     }
+	private class JSONEnviarMensaje extends AsyncTask<String, String, JSONObject> {
+		private ProgressDialog pDialog;
+		@Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+             ver = (TextView) rootView.findViewById(R.id.nombre);
+             name = (TextView) rootView.findViewById(R.id.plantel);
+             api = (TextView) rootView.findViewById(R.id.nivel);
+             pDialog = new ProgressDialog(rootView.getContext());
+             pDialog.setMessage("Enviando el mensaje...");
+             pDialog.setIndeterminate(false);
+             pDialog.setCancelable(false);
+             pDialog.show();
+		}
+		@Override
+        protected JSONObject doInBackground(String... args) {
+	        JSONParser jParser = new JSONParser();
+	        List<NameValuePair> parametros = new ArrayList<NameValuePair>();
+        	parametros.add(new BasicNameValuePair("mensaje", mensaje.getText().toString() ));
+        	parametros.add(new BasicNameValuePair("id_alumno", id_alumno));
+        	parametros.add(new BasicNameValuePair("id_tutor", id_tutor));
+	        JSONObject json = jParser.makeHttpRequest(url_mensaje,"POST", parametros);
+	        return json;
+		}
+		@Override
+        protected void onPostExecute(JSONObject json) {
+        pDialog.dismiss();
+	    	if(json != null){
+				try {
+					if (json.getString("a") =="true" ) {
+						respuesta = "insertado";
+						mensaje.setText("");
+						
+						new JSONParse().execute();
+					}else {
+						respuesta="error";
+					}
+				} catch (JSONException e) {
+					respuesta = "Error de conexión";
+				}
+			}
+			
+       }
+	}
 }
